12e714611554ccc3647167f1ade1aa9a6d320543
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2025-02-02 19:34:44 +0200

    Update tasks.py

--- ML/tasks.py
+++ ML/tasks.py
@@ -21,4 +21,4 @@ def moving_average(series, window_size):
 # Example usage
 time_series = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 window = 3
-print(moving_average(time_series, window))
+print(moving_average(time_series, window))   # [20. 30. 40. 50. 60. 70. 80. 90.]
